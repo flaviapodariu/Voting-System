9bbdb0786e01b4ef9d9d0dd45b74ef0352c0b0d4
Liviu Gheorghe <gheorghel035@gmail.com>
2025-01-12 11:25:07 +0200

    UI boilerplate

---
+++ ui/src/components/Layout/Footer/Footer.tsx
@@ -0,0 +1,8 @@
+import HeartIcon from 'assets/img/heart.svg?react';
+
+export const Footer = () => {
+  return (
+    <footer className='mx-auto w-full max-w-prose pb-6 pl-6 pr-6 text-center text-gray-400'>
+    </footer>
+  );
+};
